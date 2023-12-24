66a913baaf44b487c15311ffd8f45c4ed5fbbdf5
patrick okafor <patrickpeters911@gmail.com>
2023-12-23 17:54:33 -0800

    a function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,10 +28,11 @@ void swap(int *a, int *b)
 
 int partition(int arr[], int low, int high, size_t size)
 {
+	int j;
 	int pivot = arr[high];
 	int i = (low - 1);
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (arr[j] < pivot)
 		{
